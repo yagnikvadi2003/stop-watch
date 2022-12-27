3edb1a8214ebd417e8449a111bb15eb2de489eb7
Vadi Yagnik <99962442+yagnikvadi2003@users.noreply.github.com>
2022-12-27 23:18:58 +0530

    stop (#1)
    stop-watch bug fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import React, { useState } from 'react'
 import './App.css';
+import Timer from './components/Timer';
 
-let adjestInterval = undefined;
-function App() {
-  const [watch, setWatch] = useState(0);
-  const [stated, setStated] = useState(false);
-
-  const startWatch = ()=> {
-    adjestInterval = setInterval(()=>{
-      setWatch((x)=>x+1)
-    },1000)
-    setStated(true);
-  }
-  const stopWatch = ()=> {
-    clearInterval(adjestInterval);
-    setStated(false);
-  }
-  const resetWatch = ()=> {
-    setWatch(0);
-    clearInterval(adjestInterval);
-    setStated(false);
-  }
-
+const App = () => {
   return (
-    <div className="App">
-      <h1>Start Stop Watch</h1>
-      <h1>{watch}</h1>
-      <button className='start' disabled={stated} onClick={startWatch}>Start</button>
-      <button className='stop' onClick={stopWatch}>Stop</button>
-      <button className='reset' onClick={resetWatch}>Reset</button>
-    </div>
+    <Timer />
   );
 }
 
